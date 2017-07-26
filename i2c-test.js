c93ebd337824b93bde84b4ffa115fd9559566c38
Unknown <pety.mina@gmail.com>
2017-07-26 18:16:48 +0200

    test formatovani

--- i2c-test.js
+++ i2c-test.js
@@ -1,36 +1,19 @@
 var exec = require('exec');
 
-function getTime(adress) {
   
-  exec(['i2cget', '-y', '0', '0x68', '0x02'], function(err, out, code) {
-  if (err instanceof Error)
-    throw err;
-  
+exec(['i2cget', '-y', '0', '0x68', '0x02'], function(err, out, code) 
+{
   var hour = out;
-
-  exec(['i2cget', '-y', '0', '0x68', '0x01'], function(err, out, code) {
-  if (err instanceof Error)
-    throw err;
-
-  var minuts = out;
-
-    exec(['i2cget', '-y', '0', '0x68', '0x00'], function(err, out, code) {
-  if (err instanceof Error)
-    throw err;
-
-  var seconds = out;
-
-	process.stdout.write(hour);
-        process.stdout.write(minuts);
-        process.stdout.write(seconds);
-	 process.exit(code);
-
-  }
-
-
-  }
-
-
-  }
-
+  exec(['i2cget', '-y', '0', '0x68', '0x01'], function(err, out, code) 
+  {
+    var minuts = out;
+    exec(['i2cget', '-y', '0', '0x68', '0x00'], function(err, out, code) 
+    {
+      var seconds = out;
+      process.stdout.write(hour);
+      process.stdout.write(minuts);
+      process.stdout.write(seconds);
+      process.exit(code);
+    })
+  })
 });
